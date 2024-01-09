db6163bd5224a58d449a43611962287cfdf35193
Ian <109614480+itsHackinTime@users.noreply.github.com>
2024-01-09 14:07:12 -0600

    add navbar links

--- personalsite/src/app/page.tsx
+++ personalsite/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        
+        <Link href="/">Home</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
       </div>
     </main>
   )
