c8f421a68d920f3cf9af9e090b244526942e1777
Ian <109614480+itsHackinTime@users.noreply.github.com>
2024-01-11 11:08:09 -0600

    added styling to navbar

--- personalsite/src/app/navbar.tsx
+++ personalsite/src/app/navbar.tsx
@@ -2,11 +2,12 @@ import Link from "next/link"
 import styles from "./navbar.module.scss"
 export const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <Link href="/">Home</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/contact">Contact</Link>
-    </div>
+      <div className={styles.navbar}>
+        <Link href="/">Home</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+  
   )
 }
 
