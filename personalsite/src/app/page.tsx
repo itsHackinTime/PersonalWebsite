1693ff7a496eeecc84618b17f95efb8f6d3d3f01
Ian <109614480+itsHackinTime@users.noreply.github.com>
2024-03-06 15:03:31 -0600

    added about me section

--- personalsite/src/app/page.tsx
+++ personalsite/src/app/page.tsx
@@ -15,6 +15,13 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className='rightroundedaccent'>
+        <div className='aboutme'>
+            <p>
+              Hello world! Im Ian Weinholtz, A proficient Full-Stack Developer based in St. Louis, Missouri, with specialization in React, Typescript, and Express, complemented by a solid foundation in GraphQL, SQL databases like PostgreSQL, and NoSQL databases like MongoDB. Experience includes developing web applications, enhancing software performance through caching solutions like Redis, and improving code maintainability. Significant contributions have been made to open-source projects, notably enhancing the Quell library by optimizing GraphQL query caching and increasing test coverage. Projects highlight the ability to develop efficient, user-centric applications using modern web technologies and security practices. Educational background from Webster University and hands-on software development skills demonstrate a valuable asset to tech teams, as shown by a dynamic project portfolio and active GitHub contributions.
+            </p>
+        </div>
+      </div>
     </main>
   )
 } 
